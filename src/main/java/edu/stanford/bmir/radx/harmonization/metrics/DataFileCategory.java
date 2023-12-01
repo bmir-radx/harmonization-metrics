0eef0c19461c78ea82d18d54ecd2130dc2462279
jkyu <jkyu@stanford.edu>
2023-12-01 13:55:23 -0800

    added enum for data file categories orig and transform

--- src/main/java/edu/stanford/bmir/radx/harmonization/metrics/DataFileCategory.java
+++ src/main/java/edu/stanford/bmir/radx/harmonization/metrics/DataFileCategory.java
@@ -1,2 +1,30 @@
-package edu.stanford.bmir.radx.harmonization.metrics;public enum DataFileCategory {
+package edu.stanford.bmir.radx.harmonization.metrics;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum DataFileCategory {
+    ORIG("orig"),
+    TRANSFORM("transform");
+
+    private String text;
+
+    DataFileCategory(String text) {
+        this.text = text;
+    }
+
+    private static final Map<String, DataFileCategory> textToCategory;
+
+    static {
+        textToCategory = new HashMap<>();
+        textToCategory.put(ORIG.text, ORIG);
+        textToCategory.put(TRANSFORM.text, TRANSFORM);
+    }
+
+    public static DataFileCategory fromString(String text) throws InvalidDataFileCategoryException {
+        if (!textToCategory.containsKey(text)) {
+            throw new InvalidDataFileCategoryException(text);
+        }
+        return textToCategory.get(text);
+    }
 }
