939d59dbaf36a60e9671eeba8d48403a4f30f9e0
jkyu <jkyu@stanford.edu>
2023-12-06 11:40:13 -0800

    fixed typing in OrigFile and TransformFile tests

--- src/test/java/edu/stanford/bmir/radx/harmonization/metrics/OrigFileTest.java
+++ src/test/java/edu/stanford/bmir/radx/harmonization/metrics/OrigFileTest.java
@@ -12,17 +12,17 @@ public class OrigFileTest {
     public void testGetVersion() {
         int version = 1;
         HashSet<String> variableNames = new HashSet<>();
-        DataFile dataFile = new OrigFile("filename", version, variableNames);
+        OrigFile origFile = new OrigFile("filename", version, variableNames);
 
-        assertEquals(version, dataFile.version());
+        assertEquals(version, origFile.version());
     }
 
     @Test
     public void testGetVariableNames() {
         int version = 1;
         HashSet<String> variableNames = new HashSet<>(Arrays.asList("var1", "var2"));
-        DataFile dataFile = new OrigFile("filename", version, variableNames);
+        OrigFile origFile = new OrigFile("filename", version, variableNames);
 
-        assertEquals(variableNames, dataFile.variableNames());
+        assertEquals(variableNames, origFile.variableNames());
     }
 }
--- src/test/java/edu/stanford/bmir/radx/harmonization/metrics/TransformFileTest.java
+++ src/test/java/edu/stanford/bmir/radx/harmonization/metrics/TransformFileTest.java
@@ -12,17 +12,17 @@ public class TransformFileTest {
     public void testGetVersion() {
         int version = 1;
         HashSet<String> variableNames = new HashSet<>();
-        DataFile dataFile = new TransformFile("filename", version, variableNames);
+        TransformFile transformFile = new TransformFile("filename", version, variableNames);
 
-        assertEquals(version, dataFile.version());
+        assertEquals(version, transformFile.version());
     }
 
     @Test
     public void testGetVariableNames() {
         int version = 1;
         HashSet<String> variableNames = new HashSet<>(Arrays.asList("var1", "var2"));
-        DataFile dataFile = new TransformFile("filename", version, variableNames);
+        TransformFile transformFile = new TransformFile("filename", version, variableNames);
 
-        assertEquals(variableNames, dataFile.variableNames());
+        assertEquals(variableNames, transformFile.variableNames());
     }
 }
