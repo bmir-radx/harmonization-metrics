cc5715f09170347fd5f1ea008ba726b4787ae87c
jkyu <jkyu@stanford.edu>
2023-12-06 11:52:33 -0800

    replace wildcard imports

--- src/test/java/edu/stanford/bmir/radx/harmonization/metrics/OrigTransformFilePairMetricsTest.java
+++ src/test/java/edu/stanford/bmir/radx/harmonization/metrics/OrigTransformFilePairMetricsTest.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class OrigTransformFilePairMetricsTest {
 
