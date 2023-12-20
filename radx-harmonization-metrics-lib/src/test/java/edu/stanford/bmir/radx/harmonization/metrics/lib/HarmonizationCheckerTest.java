ab1cd35cbbe3e3ed805cfc470f6f85a7d0b55ae6
jkyu <jkyu@stanford.edu>
2023-12-20 14:13:17 -0800

    updated tests on HarmonizationChecker to account for tiered variables

--- radx-harmonization-metrics-lib/src/test/java/edu/stanford/bmir/radx/harmonization/metrics/lib/HarmonizationCheckerTest.java
+++ radx-harmonization-metrics-lib/src/test/java/edu/stanford/bmir/radx/harmonization/metrics/lib/HarmonizationCheckerTest.java
@@ -4,46 +4,99 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 public class HarmonizationCheckerTest {
 
-    @Test
-    public void testCountHarmonizableElements() throws InvalidProgramIdException {
-        ProgramId programId = ProgramId.RADXUP;
-        HarmonizationRules rules = new MockRules();
-        HarmonizationChecker checker = new HarmonizationChecker(rules);
+    private Set<String> generateHarmonizableVariables() {
+        return new HashSet<>(
+                Arrays.asList("t1Var1", "t1Var2", "t1Var3", "t2Var1", "t2Var2", "t3Var1", "trash"));
+    }
 
-        HashSet<String> nonHarmonizedVariables = new HashSet<>(Arrays.asList("var1", "var2", "var4"));
-        int nHarmonizableElements = checker.countHarmonizableElements(programId, nonHarmonizedVariables);
-        assertEquals(2, nHarmonizableElements);
-        assertNotEquals(nonHarmonizedVariables.size(), nHarmonizableElements);
+    private Set<String> generateHarmonizedVariables() {
+        Set<String> variables = new HashSet<>();
+        variables.add("harmonizedT1Var1");
+        variables.add("harmonizedT1Var2");
+        variables.add("harmonizedT1Var3");
+        variables.add("harmonizedT2Var1");
+        variables.add("harmonizedT2Var2");
+        variables.add("harmonizedT3Var3");
+        variables.add("trash");
+        return variables;
+    }
+
+    private Set<String> generateMixedVariables() {
+        Set<String> variables = generateHarmonizableVariables();
+        variables.addAll(generateHarmonizedVariables());
+        return variables;
     }
 
     @Test
-    public void testCountHarmonizedElements() throws InvalidProgramIdException {
-        ProgramId programId = ProgramId.RADXUP;
+    public void testCountHarmonizableElements()
+            throws InvalidProgramIdException, InvalidHarmonizationTierException {
+        ProgramId programId = ProgramId.DHT;
         HarmonizationRules rules = new MockRules();
         HarmonizationChecker checker = new HarmonizationChecker(rules);
 
-        HashSet<String> variables = new HashSet<>(Arrays.asList("var1", "harmonizedVar2", "harmonizedVar3"));
-        int nHarmonizedElements = checker.countHarmonizedElements(programId, variables);
-        assertEquals(2, nHarmonizedElements);
-        assertNotEquals(variables.size(), nHarmonizedElements);
+        Set<String> variables = generateHarmonizableVariables();
+        int nHarmonizableTier1 = checker.countHarmonizableElements(
+                programId, variables, HarmonizationTier.TIER1);
+        int nHarmonizableTier2 = checker.countHarmonizableElements(
+                programId, variables, HarmonizationTier.TIER2);
+        int nHarmonizableTier3 = checker.countHarmonizableElements(
+                programId, variables, HarmonizationTier.TIER3);
+        assertEquals(3, nHarmonizableTier1);
+        assertEquals(2, nHarmonizableTier2);
+        assertEquals(1, nHarmonizableTier3);
     }
 
     @Test
-    public void testHarmonizationCounts() throws InvalidProgramIdException {
-        ProgramId programId = ProgramId.RADXUP;
+    public void testCountHarmonizedElements()
+            throws InvalidProgramIdException, InvalidHarmonizationTierException {
+        ProgramId programId = ProgramId.DHT;
         HarmonizationRules rules = new MockRules();
         HarmonizationChecker checker = new HarmonizationChecker(rules);
 
-        HashSet<String> variables = new HashSet<>(Arrays.asList("var1", "harmonizedVar2", "harmonizedVar3", "var4"));
-        int nHarmonizableElements = checker.countHarmonizableElements(programId, variables);
-        int nHarmonizedElements = checker.countHarmonizedElements(programId, variables);
-        assertEquals(1, nHarmonizableElements);
-        assertEquals(2, nHarmonizedElements);
+        Set<String> variables = generateHarmonizedVariables();
+        int nHarmonizedTier1 = checker.countHarmonizedElements(
+                programId, variables, HarmonizationTier.TIER1);
+        int nHarmonizedTier2 = checker.countHarmonizedElements(
+                programId, variables, HarmonizationTier.TIER2);
+        int nHarmonizedTier3 = checker.countHarmonizedElements(
+                programId, variables, HarmonizationTier.TIER3);
+        assertEquals(3, nHarmonizedTier1);
+        assertEquals(2, nHarmonizedTier2);
+        assertEquals(1, nHarmonizedTier3);
+    }
+
+    @Test
+    public void testHarmonizationCounts()
+            throws InvalidProgramIdException, InvalidHarmonizationTierException {
+        ProgramId programId = ProgramId.DHT;
+        HarmonizationRules rules = new MockRules();
+        HarmonizationChecker checker = new HarmonizationChecker(rules);
+
+        Set<String> variables = generateMixedVariables();
+        int nHarmonizableTier1 = checker.countHarmonizableElements(
+                programId, variables, HarmonizationTier.TIER1);
+        int nHarmonizableTier2 = checker.countHarmonizableElements(
+                programId, variables, HarmonizationTier.TIER2);
+        int nHarmonizableTier3 = checker.countHarmonizableElements(
+                programId, variables, HarmonizationTier.TIER3);
+        int nHarmonizedTier1 = checker.countHarmonizedElements(
+                programId, variables, HarmonizationTier.TIER1);
+        int nHarmonizedTier2 = checker.countHarmonizedElements(
+                programId, variables, HarmonizationTier.TIER2);
+        int nHarmonizedTier3 = checker.countHarmonizedElements(
+                programId, variables, HarmonizationTier.TIER3);
+        assertEquals(6, nHarmonizableTier1);
+        assertEquals(4, nHarmonizableTier2);
+        assertEquals(2, nHarmonizableTier3);
+        assertEquals(3, nHarmonizedTier1);
+        assertEquals(2, nHarmonizedTier2);
+        assertEquals(1, nHarmonizedTier3);
     }
 }
