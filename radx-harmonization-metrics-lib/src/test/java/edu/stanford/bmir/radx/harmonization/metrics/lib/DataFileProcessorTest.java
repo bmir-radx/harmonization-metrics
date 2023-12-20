b55902bd2d9099fda7c70e1909370f1e9d14367d
jkyu <jkyu@stanford.edu>
2023-12-20 15:26:19 -0800

    added test for generating Study objects from OrigTransformFilePairs

--- radx-harmonization-metrics-lib/src/test/java/edu/stanford/bmir/radx/harmonization/metrics/lib/DataFileProcessorTest.java
+++ radx-harmonization-metrics-lib/src/test/java/edu/stanford/bmir/radx/harmonization/metrics/lib/DataFileProcessorTest.java
@@ -2,6 +2,7 @@ package edu.stanford.bmir.radx.harmonization.metrics.lib;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -14,33 +15,58 @@ public class DataFileProcessorTest {
             new FileNameExtractor(),
             new VariableNameCleaner());
 
-    private List<DataFileExternal> generateExternalDataForTest() {
+    private List<DataFileExternal> generateExternalDataForTest(String studyId) {
         String program = "RADx-UP";
-        String studyId = "study123";
         List<String> variables = Arrays.asList("var1", "var2", "var3");
         DataFileExternal dataExt1 = new DataFileExternal(
-                "file1_DATA_origcopy_v1.csv", program, studyId, "orig", variables);
+                String.format("%s_file1_DATA_origcopy_v1.csv", studyId),
+                program, studyId, "orig", variables);
         DataFileExternal dataExt2 = new DataFileExternal(
-                "file1_DATA_origcopy_v2.csv", program, studyId, "orig", variables);
+                String.format("%s_file1_DATA_origcopy_v2.csv", studyId),
+                program, studyId, "orig", variables);
         DataFileExternal dataExt3 = new DataFileExternal(
-                "file1_DATA_transformcopy_v2.csv", program, studyId, "transform", variables);
+                String.format("%s_file1_DATA_transformcopy_v2.csv", studyId),
+                program, studyId, "transform", variables);
         DataFileExternal dataExt4 = new DataFileExternal(
-                "file1_DATA_transformcopy_v4.csv", program, studyId, "transform", variables);
+                String.format("%s_file1_DATA_transformcopy_v4.csv", studyId),
+                program, studyId, "transform", variables);
         DataFileExternal dataExt5 = new DataFileExternal(
-                "file5_DATA_transformcopy_v4.csv", program, studyId, "transform", variables);
+                String.format("%s_file5_DATA_transformcopy_v4.csv", studyId),
+                program, studyId, "transform", variables);
         List<DataFileExternal> dataFiles = Arrays.asList(dataExt1, dataExt2, dataExt3, dataExt4, dataExt5);
         return dataFiles;
     }
 
     @Test
-    public void testProcessDataFiles()
-            throws InvalidOrigTransformCategoryException, InvalidProgramIdException, NoVersionNumberException {
-        List<DataFileExternal> externalDataFiles = generateExternalDataForTest();
+    public void processDataFiles()
+            throws InvalidOrigTransformCategoryException, InvalidProgramIdException,
+            NoVersionNumberException {
+        List<DataFileExternal> externalDataFiles = generateExternalDataForTest("study1");
         Map<ReducedFileName, OrigTransformFilePair> dataFilePairMap = processor.processDataFiles(externalDataFiles);
-        ReducedFileName expectedKey1 = new ReducedFileName("file1");
-        ReducedFileName expectedKey2 = new ReducedFileName("file5");
+        ReducedFileName expectedKey1 = new ReducedFileName("study1_file1");
+        ReducedFileName expectedKey2 = new ReducedFileName("study1_file5");
         assertEquals(2, dataFilePairMap.size());
         assertTrue(dataFilePairMap.containsKey(expectedKey1));
         assertTrue(dataFilePairMap.containsKey(expectedKey2));
+    }
+
+    @Test
+    public void organizeFilePairsByStudy()
+            throws NoVersionNumberException, InvalidOrigTransformCategoryException,
+            InvalidProgramIdException {
+        List<DataFileExternal> externalDataFiles = new ArrayList<>();
+        StudyId study1 = StudyId.valueOf("study1");
+        StudyId study2 = StudyId.valueOf("study2");
+        StudyId study3 = StudyId.valueOf("study3");
+        externalDataFiles.addAll(generateExternalDataForTest("study1"));
+        externalDataFiles.addAll(generateExternalDataForTest("study2"));
+        externalDataFiles.addAll(generateExternalDataForTest("study3"));
+        Map<ReducedFileName, OrigTransformFilePair> dataFilePairMap = processor.processDataFiles(externalDataFiles);
+        Map<StudyId, Study> studyMap = processor.organizeFilePairsByStudy(dataFilePairMap);
+        assertEquals(3, studyMap.size());
+        assertTrue(studyMap.containsKey(study1));
+        assertTrue(studyMap.containsKey(study2));
+        assertTrue(studyMap.containsKey(study3));
+
     }
 }
