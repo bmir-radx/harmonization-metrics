be8d2c5255e503d16c966d60212ed263d1b20cf1
jkyu <jkyu@stanford.edu>
2024-01-04 16:19:55 -0800

    added a csv writer

--- radx-harmonization-metrics-app/src/main/java/edu/stanford/bmir/radx/harmonization/metrics/app/CalculateMetricsCommand.java
+++ radx-harmonization-metrics-app/src/main/java/edu/stanford/bmir/radx/harmonization/metrics/app/CalculateMetricsCommand.java
@@ -1,5 +1,6 @@
 package edu.stanford.bmir.radx.harmonization.metrics.app;
 
+import edu.stanford.bmir.radx.harmonization.metrics.lib.CsvWriter;
 import edu.stanford.bmir.radx.harmonization.metrics.lib.DataFileExternal;
 import edu.stanford.bmir.radx.harmonization.metrics.lib.MetricsCalculator;
 import edu.stanford.bmir.radx.harmonization.metrics.lib.MetricsReport;
@@ -7,6 +8,7 @@ import edu.stanford.bmir.radx.harmonization.metrics.lib.TrialDataProcessor;
 import org.springframework.stereotype.Component;
 import picocli.CommandLine;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.Callable;
 
@@ -19,20 +21,33 @@ public class CalculateMetricsCommand implements Callable<Integer> {
 
     private final MetricsCalculator metricsCalculator;
     private final TrialDataProcessor trialDataProcessor;
+    private final CsvWriter csvWriter;
 
     @CommandLine.Option(names = {"-f", "--file"}, required = true, description = "Path to trial data to read in.")
     protected String fileName;
 
-    public CalculateMetricsCommand(MetricsCalculator metricsCalculator, TrialDataProcessor trialDataProcessor) {
+    @CommandLine.Option(
+            names = {"-os", "--output-studies"}, required = true,
+            description = "Path of CSV to write study metrics.")
+    protected String studyCsv;
+
+    @CommandLine.Option(
+            names = {"-op", "--output-pairs"}, required = true,
+            description = "Path of CSV to write orig-transform file pair metrics.")
+    protected String pairCsv;
+
+    public CalculateMetricsCommand(MetricsCalculator metricsCalculator, TrialDataProcessor trialDataProcessor, CsvWriter csvWriter) {
         this.metricsCalculator = metricsCalculator;
         this.trialDataProcessor = trialDataProcessor;
+        this.csvWriter = csvWriter;
     }
 
     @Override
     public Integer call() throws Exception {
         List<DataFileExternal> externalData = trialDataProcessor.readExternalData(fileName);
         MetricsReport metrics = metricsCalculator.computeHarmonizationMetrics(externalData);
-        System.out.println(metrics);
+        csvWriter.writeStudyReport(metrics.studyMetrics(), studyCsv);
+        csvWriter.writeFilePairReport(metrics.pairMetrics(), pairCsv);
         return 0;
     }
 }
---
+++ radx-harmonization-metrics-lib/src/main/java/edu/stanford/bmir/radx/harmonization/metrics/lib/CsvWriter.java
@@ -0,0 +1,122 @@
+package edu.stanford.bmir.radx.harmonization.metrics.lib;
+
+import com.opencsv.CSVWriter;
+import org.springframework.stereotype.Component;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class CsvWriter {
+
+    public void writeStudyReport(List<StudyMetrics> studyMetrics, String filePath) throws IOException {
+        try (CSVWriter writer = new CSVWriter(new FileWriter(filePath))) {
+            String[] headers = {
+                    "Study ID",
+                    "DCC",
+                    "Number Orig-Transform Pairs",
+                    "Unique Variables",
+                    "Unique Harmonizable Variables (Tier-1)",
+                    "Unique Harmonizable Variables (Tier-2)",
+                    "Unique Harmonizable Variables (Tier-3)",
+                    "% Harmonizable",
+                    "Unique Harmonized Variables (Tier-1)",
+                    "Unique Harmonized Variables (Tier-2)",
+                    "Unique Harmonized Variables (Tier-3)",
+                    "% Harmonized"
+            };
+            writer.writeNext(headers);
+
+            for (var metrics : studyMetrics) {
+                String[] line = getStudyRow(metrics);
+                writer.writeNext(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeFilePairReport(List<OrigTransformFilePairMetrics> pairMetrics, String filePath) throws IOException {
+        try (CSVWriter writer = new CSVWriter(new FileWriter(filePath))) {
+            String[] headers = {
+                    "File Name (Orig)",
+                    "File Name (Transform)",
+                    "Study ID",
+                    "DCC",
+                    "Unique Variables (Orig)",
+                    "Unique Variables (Transform)",
+                    "Unique Harmonizable Variables (Tier-1)",
+                    "Unique Harmonizable Variables (Tier-2)",
+                    "Unique Harmonizable Variables (Tier-3)",
+                    "% Harmonizable",
+                    "Unique Harmonized Variables (Tier-1)",
+                    "Unique Harmonized Variables (Tier-2)",
+                    "Unique Harmonized Variables (Tier-3)",
+                    "% Harmonized"
+            };
+            writer.writeNext(headers);
+
+            for (var metrics : pairMetrics) {
+                String[] line = getPairRow(metrics);
+                writer.writeNext(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String[] getPairRow(OrigTransformFilePairMetrics metrics) {
+        Double percentHarmonizable = ((double) (metrics.nHarmonizableDataElementsTier1()
+                + metrics.nHarmonizableDataElementsTier2()
+                + metrics.nHarmonizableDataElementsTier3())
+                / Math.max(metrics.nDataElementsOrig(), metrics.nDataElementsTransform()));
+        Double percentHarmonized = ((double) (metrics.nHarmonizedDataElementsTier1()
+                + metrics.nHarmonizedDataElementsTier2()
+                + metrics.nHarmonizedDataElementsTier3())
+                / Math.max(metrics.nDataElementsOrig(), metrics.nDataElementsTransform()));
+        String[] line = {
+                metrics.origFileName().orElse(null),
+                metrics.transformFileName().orElse(null),
+                metrics.studyId().value(),
+                metrics.programId().toString(),
+                String.valueOf(metrics.nDataElementsOrig()),
+                String.valueOf(metrics.nDataElementsTransform()),
+                String.valueOf(metrics.nHarmonizableDataElementsTier1()),
+                String.valueOf(metrics.nHarmonizableDataElementsTier2()),
+                String.valueOf(metrics.nHarmonizableDataElementsTier3()),
+                String.valueOf(percentHarmonizable),
+                String.valueOf(metrics.nHarmonizedDataElementsTier1()),
+                String.valueOf(metrics.nHarmonizedDataElementsTier2()),
+                String.valueOf(metrics.nHarmonizedDataElementsTier3()),
+                String.valueOf(percentHarmonized)
+        };
+        return line;
+    }
+
+    private String[] getStudyRow(StudyMetrics metrics) {
+        Double percentHarmonizable = ((double) (metrics.nUniqueHarmonizableDataElementsTier1()
+                + metrics.nUniqueHarmonizableDataElementsTier2()
+                + metrics.nUniqueHarmonizableDataElementsTier3())
+                / metrics.nUniqueDataElements());
+        Double percentHarmonized = ((double) (metrics.nUniqueHarmonizedDataElementsTier1()
+                + metrics.nUniqueHarmonizedDataElementsTier2()
+                + metrics.nUniqueHarmonizedDataElementsTier3())
+                / metrics.nUniqueDataElements());
+        String[] line = {
+                metrics.studyId().value(),
+                metrics.programId().toString(),
+                String.valueOf(metrics.nOrigTransformFilePairs()),
+                String.valueOf(metrics.nUniqueDataElements()),
+                String.valueOf(metrics.nUniqueHarmonizableDataElementsTier1()),
+                String.valueOf(metrics.nUniqueHarmonizableDataElementsTier3()),
+                String.valueOf(metrics.nUniqueHarmonizableDataElementsTier3()),
+                String.valueOf(percentHarmonizable),
+                String.valueOf(metrics.nUniqueHarmonizedDataElementsTier1()),
+                String.valueOf(metrics.nUniqueHarmonizedDataElementsTier2()),
+                String.valueOf(metrics.nUniqueHarmonizedDataElementsTier3()),
+                String.valueOf(percentHarmonized)
+        };
+        return line;
+    }
+}
